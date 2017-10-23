fa351d8c4c192c7a781650272d5882c9a8af8c4c
mallika <mallikapodapati568@gmail.com>
2017-10-23 12:35:41 +0530

    this is my Backend project

---
+++ Backend/src/main/java/com/niit/Backend/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package com.niit.Backend.dao;
+import java.util.List;
+
+import com.niit.Backend.model.Address;
+import com.niit.Backend.model.User;
+
+public interface UserDAO {
+
+	// user related operation
+	User getByEmail(String email);
+	User get(int id);
+
+	boolean add(User user);
+	
+	// adding and updating a new address
+	Address getAddress(int addressId);
+	boolean addAddress(Address address);
+	boolean updateAddress(Address address);
+	Address getBillingAddress(int userId);
+	List<Address> listShippingAddresses(int userId);
+	
+
+	
+}
